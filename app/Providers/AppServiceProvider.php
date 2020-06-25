b97c702266bd92eb0d1e039c4660b9607abb13a9
arsentiy <arsentiyscherbakov@gmail.com>
2020-06-25 16:01:12 +0300

    migrate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
-class AppServiceProvider extends ServiceProvider
-{
+use Illuminate\Support\Facades\Schema;
+class AppServiceProvider extends ServiceProvider{
     /**
      * Register any application services.
      *
@@ -23,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+       Schema::defaultStringLength(191);
     }
 }
